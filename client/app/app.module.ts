e731576a7297ec0024666c4f736629198b731dc1
David Thong Nguyen <thnguyen2808@gmail.com>
2018-05-31 10:30:49 +0700

    Initial commit

---
+++ client/app/app.module.ts
@@ -0,0 +1,46 @@
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {HttpClientModule} from '@angular/common/http';
+import {MyApp} from './app.component';
+
+import {HomePage} from '../pages/home/home';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {TabsPage} from '../pages/tabs/tabs';
+
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Geolocation} from '@ionic-native/geolocation';
+
+
+@NgModule({
+    declarations: [
+        MyApp,
+        HomePage,
+        AboutPage,
+        ContactPage,
+        TabsPage,
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        HttpClientModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        AboutPage,
+        ContactPage,
+        TabsPage,
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        Geolocation,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
+})
+export class AppModule {
+}
---
+++ client/classes/place.ts
@@ -0,0 +1,8 @@
+export class Place {
+    constructor(public id: string,
+                public name: string,
+                public address: string,
+                public type: string,
+                public point: any) {
+    }
+}
